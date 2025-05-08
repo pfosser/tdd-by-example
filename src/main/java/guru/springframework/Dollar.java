255393b5b6daacbc3a726c6f84fe6651483847d8
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2025-05-08 08:57:41 +0200

    Lesson 19. Equality for All

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -1,5 +1,7 @@
 package guru.springframework;
 
+import java.util.Objects;
+
 public class Dollar {
 
 	int amount;
@@ -11,4 +13,23 @@ public class Dollar {
 	Dollar times(int multiplier) {
 		return new Dollar(this.amount * multiplier);
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(amount);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Dollar other = (Dollar) obj;
+		return amount == other.amount;
+	}
+	
+	
 }
